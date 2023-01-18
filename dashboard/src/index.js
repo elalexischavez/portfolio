55114cbd8e12eab4b89a67103f73077f4eb7e77a
AlexChavezz <alexischavez1234.ac@gmail.com>
2023-01-18 16:48:42 -0600

    login-layout updated

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -7,10 +7,41 @@
         e.preventDefault();
         const userName = document.querySelector('#userName').value;
         const userPassword = document.querySelector('#password').value;
-        getdata({ userName, password: userPassword }).then(console.log)
+        insertSpinner();
+        getdata({ userName, password: userPassword }).then((res) => {
+            document.querySelector('.loader').remove();
+            if(res.message)
+            {
+                return showAlert(res.message);
+            }
+            // codigo ->
+            const mainContainer = document.createElement('div');
+            document.querySelector('form').remove();
+            document.querySelector('main').appendChild(mainContainer);
+            mainContainer.classList.add('main-container');
+        })
     })
 })()
 
+
+function insertSpinner() {
+    const loader = document.createElement('div');
+    const loadercontainer = document.createElement('div');
+    loadercontainer.appendChild(loader);
+    loadercontainer.classList.add('loaderContainer');
+    loader.classList.add('loader');
+    document.querySelector('form').appendChild(loadercontainer);
+}
+
+function showAlert(message) {
+    const alert = document.createElement('div');
+    alert.classList.add('alert');
+    alert.innerHTML = message;
+    document.querySelector('form').appendChild(alert);
+    setTimeout(() => {
+        alert.remove();
+    }, 3000);
+}
 
 // -> promise
 async function getdata({ userName, password }) {
